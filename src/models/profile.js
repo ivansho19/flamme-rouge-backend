64adb611a7a9b0d4b744c9a38f8a3cab718ee88b
Ivan Manrique <imanrique@seek.pe>
2026-03-15 18:03:23 -0400

    update schema profile

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,28 +1,94 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-const profileSchema = new mongoose.Schema(
-  { 
-    objectId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Client" },
-    displayName: { type: String, required: true },
-    bio: { type: String, required: false },
-    phone: { type: Number, required: false },
-    city: { type: String, required: false },
-    availabity: { type: [String], required: false },
-    gender: { type: String, required: false },
-    age: { type: Number, required: false },
-    nationality: { type: String, required: false },
-    height: { type: Number, required: false },
-    weight: { type: Number, required: false },
-    haircolor: { type: String, required: false },
-    eyecolor: { type: String, required: false },
-    language: { type: [String], required: false },
-    isPremium: { type: Boolean, default: false },
-    imagesMain: { type: String, required: false },
-    imagesGallery: { type: [String], required: false },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+const ImageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
   },
-  { timestamps: true }
-);
+  public_id: {
+    type: String,
+    required: true
+  }
+});
 
-export default mongoose.model("Profile", profileSchema);
+const ProfileSchema = new mongoose.Schema({
+
+  objectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Client"
+  },
+
+  displayName: {
+    type: String,
+    required: true
+  },
+
+  bio: {
+    type: String
+  },
+
+  phone: {
+    type: String
+  },
+
+  city: {
+    type: String
+  },
+
+  availability: {
+    type: [String]
+  },
+
+  gender: {
+    type: String
+  },
+
+  age: {
+    type: Number
+  },
+
+  nationality: {
+    type: String
+  },
+
+  height: {
+    type: Number
+  },
+
+  weight: {
+    type: Number
+  },
+
+  hairColor: {
+    type: String
+  },
+
+  eyeColor: {
+    type: String
+  },
+
+  languages: {
+    type: [String]
+  },
+
+  isPremium: {
+    type: Boolean,
+    default: false
+  },
+
+  imagesMain: {
+    type: ImageSchema,
+    required: false
+  },
+
+  imagesGallery: {
+    type: [ImageSchema],
+    default: []
+  }
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Profile', ProfileSchema);
