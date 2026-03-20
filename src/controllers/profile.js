a392c00999eb171d1e2c5826b5654922b442890c
Ivan Manrique <imanrique@seek.pe>
2026-03-19 22:34:38 -0400

    feature: add sort in function getAllProfiles

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -97,7 +97,7 @@ export const getProfileByID = async (req, res) => {
 // Buscar todos los profiles
 export const getAllProfiles = async (req, res) => {
   try {
-    const profiles = await Profile.find();
+    const profiles = await Profile.find().sort({ createdAt: -1 });
     res.status(200).json(profiles);
   } catch (error) {
     console.log("Error en getAllProfiles:", error);
