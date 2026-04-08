e0bb4f559d79f6150405f92e83e3a05ce5a0bde2
Ivan Manrique <imanrique@seek.pe>
2026-04-08 11:11:13 -0400

    add new config CORS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import profilesRoutes from "./routes/profilesRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
